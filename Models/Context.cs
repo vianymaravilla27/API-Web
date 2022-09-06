783eef852f42cd32b523838aaec4d17c3bf6c02d
Vianey Maravilla <112917745+vianymaravilla27@users.noreply.github.com>
2022-09-05 22:31:47 -0500

    Add files via upload

---
+++ Models/Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Web_Api.Models
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Item> Items { get; set; } = null!;
+    }
+}
